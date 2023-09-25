f9926ab9a253203743c5c2c1a527ddd774fe4fb0
Dominic Valentino <Dominic.Valentino@blueinnovationsgroup.com>
2023-09-25 17:52:06 -0400

    add puml

--- code/examples/tea_timer/user_interface.h
+++ code/examples/tea_timer/user_interface.h
@@ -14,7 +14,48 @@ typedef struct
 }UserInterface;
 
 
+/*
 
+@startuml
+
+skin rose
+
+title User Interface
+
+state Idle
+Idle: enter: All LEDs Off
+state Config{
+  state Black :enter:Red LED, tea_type = Black
+  state Oolong :enter:Yellow LED, tea_type = Oolong
+  state Green :enter: Green LED, tea_type = Green
+  Black -> Oolong : BUTTON_PRESS
+  Oolong --> Green : BUTTON_PRESS
+  Green --> Black : BUTTON_PRESS
+}
+state Brewing{
+  Brewing: enter: Start LED timer, Send Start to Brew Control
+  Brewing: BREW_STATUS: Update status 
+  Brewing: exit: Stop LED timer
+}
+state Done
+Done: enter: Turn all LEDs on
+
+[*] --> Idle: Subscribe to Button Bus
+Idle --> Brewing: BUTTON_RELEASE
+Brewing --> Done: BREW_COMPLETE
+Done --> Idle: BUTTON_RELEASE
+state c <<choice>>
+Idle -> c: BUTTON_HELD
+c --> Black: tea_type == Black
+c --> Oolong: tea_type == Oolong
+c --> Green: tea_type == Green
+Config -> Idle : TIMEOUT
+
+@enduml
+
+
+
+*/
 
 void ui_ctor(UserInterface* self, OmBus* button_bus, BrewControl* brew_control, OmTrace* trace);
 
